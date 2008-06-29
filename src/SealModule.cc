96a26a8c1ca45f111c11c386fb02849472507b7e
cmsbuild <>
2008-06-29 15:40:47 +0000

    This commit was manufactured by cvs2git to create tag 'EventFilter- ESRawToDigi-CMSSW_2_1_0_pre9'.
    Sprout from master 2007-07-06 20:23:06 UTC Chia-Ming Kuo <sha1-1540d18cc0bb68ff0746e8491e1d31e5651fe08e@cern.ch> 'changes for 16x'
    Cherrypick from master 2007-12-31 18:43:10 UTC Natalia Ratnikova <sha1-eabc1741ce19fdddc972ebf366f29d441dd8c670@cern.ch> 'QA: cleanup of try/catch block around getByLabel.':
        data/esRawToDigi.cfi
        src/ESRawToDigi.cc
        src/ESRawToDigiCT.cc
        src/ESRawToDigiTB.cc
    Cherrypick from master 2008-06-29 15:40:43 UTC Peter Elmer <sha1-80460c2ac58e1aa0fd289ec2aa9d168a17bd6eb2@cern.ch> 'Get rid of old #include "PluginManager/ModuleDef.h"':
        python/__init__.py
        python/esRawToDigi_cfi.py
        src/SealModule.cc

--- src/SealModule.cc
+++ src/SealModule.cc
@@ -1,7 +1,4 @@
-//#include "FWCore/PluginManager/interface/ModuleDef.h"
-#include "PluginManager/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
-
 #include "EventFilter/ESRawToDigi/interface/ESRawToDigi.h"
 #include "EventFilter/ESRawToDigi/interface/ESRawToDigiTB.h"
 #include "EventFilter/ESRawToDigi/interface/ESRawToDigiCT.h"
@@ -10,5 +7,6 @@ DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(ESRawToDigi);
 DEFINE_ANOTHER_FWK_MODULE(ESRawToDigiTB);
 DEFINE_ANOTHER_FWK_MODULE(ESRawToDigiCT);
+
 
 
