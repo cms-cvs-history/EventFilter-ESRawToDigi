10871f19ed67d0aac9cc28cd0c4d9fc8b9306a95
cmsbuild <>
2008-11-13 16:09:43 +0000

    This commit was manufactured by cvs2git to create tag 'EventFilter- ESRawToDigi-V00-10-06'.
    Sprout from master 2008-11-13 16:09:41 UTC Chia-Ming Kuo <sha1-1540d18cc0bb68ff0746e8491e1d31e5651fe08e@cern.ch> 'remove dependency on TBDataFormats/ESTBRawData'
    Delete:
        data/esRawToDigi.cfi
        doc/html/index.html
        doc/html/overview.html
        interface/ESCrcKchipFast.h
        interface/ESRawToDigi.h
        interface/ESRawToDigiCT.h
        interface/ESRawToDigiTB.h
        interface/ESRecHitProducerCT.h
        interface/ESRecHitProducerTB.h
        interface/ESRecHitSimAlgoCT.h
        interface/ESRecHitSimAlgoTB.h
        interface/ESUnpacker.h
        interface/ESUnpackerCT.h
        interface/ESUnpackerTB.h
        interface/ESUnpackerV4.h
        python/__init__.py
        python/esRawToDigi_cfi.py
        src/ESRawToDigi.cc
        src/ESRawToDigiCT.cc
        src/ESRawToDigiTB.cc
        src/ESRecHitProducerCT.cc
        src/ESRecHitProducerTB.cc
        src/ESRecHitSimAlgoCT.cc
        src/ESRecHitSimAlgoTB.cc
        src/ESUnpacker.cc
        src/ESUnpackerCT.cc
        src/ESUnpackerTB.cc
        src/ESUnpackerV4.cc
        src/SealModule.cc
        test/raw2digi.cfg
        test/raw2digi.py
        test/raw2digiCT.cfg
        test/raw2digiTB.cfg
        test/raw2digiV4.cfg

--- src/SealModule.cc
+++
@@ -1,8 +0,0 @@
-#include "FWCore/Framework/interface/MakerMacros.h"
-#include "EventFilter/ESRawToDigi/interface/ESRawToDigi.h"
-
-DEFINE_SEAL_MODULE();
-DEFINE_ANOTHER_FWK_MODULE(ESRawToDigi);
-
-
-
